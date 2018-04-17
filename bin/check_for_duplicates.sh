f44f7b07d284abbc4d4aaec0dc56ff053ca8464b
Steffen Liebergeld <perl@gmx.org>
2018-04-17 21:16:38 +0200

    Add 'check_for_duplicates.sh'
    This just wraps fdupes.

---
+++ bin/check_for_duplicates.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+for tool in "fdupes";
+do
+    if ! which $tool > /dev/null;
+    then
+        echo "'$tool' not found. Exit."
+        exit 1
+    fi
+done
+
+if [[ -z "$1" ]];
+then
+    echo "Usage: $0 /path/to/target_dir"
+    exit 1
+fi
+
+if [[ ! -d "$1" ]];
+then
+    echo "Given target dir '$1' is not a directory."
+    exit 1
+fi
+
+target="$1"
+
+# ok, run fdupes and let it do its thing
+echo "Running fdupes interactively. This might take some time."
+exec fdupes -d -r "$target"
