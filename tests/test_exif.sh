04ee1d63e6ce0a431104ed6b4b8345c69ff92126
Steffen Liebergeld <perl@gmx.org>
2018-05-01 18:38:42 +0200

    complete test_exif.sh

--- tests/test_exif.sh
+++ tests/test_exif.sh
@@ -2,6 +2,7 @@
 
 tempdir=$(mktemp -d)
 datadir=$(mktemp -d)
+basedir=$(dirname $(realpath $0))
 
 trap "rm -rf -- '$tempdir' '$datadir'" EXIT
 
@@ -11,21 +12,52 @@ then
   exit
 fi
 
+logfile=$(mktemp)
+if [[ -z "$logfile" ]];
+then
+  echo "mktemp failed"
+  exit
+fi
+
+echo "Logging into $logfile"
+exec > >(tee -a "$logfile") 2>&1
+
 echo "Tempdir is $tempdir"
 echo "Datadir is $datadir"
 
-rsync -a data $datadir
+set -e # exit if a command fails
+set -x # show commands
+
+rsync -a $basedir/data $datadir
 
 echo "import 1"
-bash ../bin/exif.sh $datadir/data/import1/ $tempdir
+bash $basedir/../bin/exif.sh $datadir/data/import1/ $tempdir
 find $tempdir
 
 echo "import 2"
-bash ../bin/exif.sh $datadir/data/import2/ $tempdir
+bash $basedir/../bin/exif.sh $datadir/data/import2/ $tempdir
 find $tempdir
 
 echo "import 3"
-bash ../bin/exif.sh $datadir/data/import3/ $tempdir
-cd $tempdir
-find . -type f -ls
+bash $basedir/../bin/exif.sh $datadir/data/import3/ $tempdir
 
+cd $tempdir
+find . -type f -exec ls -dls {} \; >> $datadir/filelist.txt
+find .                             >> $datadir/filelist_paths.txt
+
+diff $datadir/filelist.txt $basedir/data/expected_filelist.txt
+if [[ $? -ne 0 ]];
+then
+  echo "Files do not sport the expected attributes. Consult $logfile"
+  exit 1
+fi
+
+diff $datadir/filelist_paths.txt $basedir/data/expected_filelist_paths.txt
+if [[ $? -ne 0 ]];
+then
+  echo "Output does not match expected paths. Consult $logfile"
+  exit 1
+fi
+
+rm $logfile
+echo "OK - Test successful"
